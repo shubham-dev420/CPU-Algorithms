4edf6b977e0f6490363d0281cb248b415f3390e0
shubham-dev420 <shubham.anand03052005@gmail.com>
2026-04-01 22:52:52 +0530

    Update fcfs.cpp

--- src/fcfs.cpp
+++ src/fcfs.cpp
@@ -1,32 +1,49 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter number of processes: ";
-    cin >> n;
+struct Process
+{
+    int pid, at, bt, ct, tat, wt;
+};
 
-    int bt[n], wt[n], tat[n];
-
-    cout << "Enter Burst Time:\n";
-    for(int i = 0; i < n; i++) {
-        cin >> bt[i];
-    }
-
-    wt[0] = 0;
-
+void find_ct(Process proc[], int n) {
+    proc[0].ct = proc[0].bt;
     for(int i = 1; i < n; i++) {
-        wt[i] = wt[i-1] + bt[i-1];
+        proc[i].ct = proc[i-1].ct + proc[i].bt;
     }
+}
 
+void find_tat(Process proc[], int n) {
     for(int i = 0; i < n; i++) {
-        tat[i] = wt[i] + bt[i];
+        proc[i].tat = proc[i].ct - proc[i].at;
     }
+}
 
-    cout << "\nProcess\tBT\tWT\tTAT\n";
-    for(int i = 0; i < n; i++) {
-        cout << i+1 << "\t" << bt[i] << "\t" << wt[i] << "\t" << tat[i] << endl;
+void find_wt(Process proc[], int n) {
+    proc[0].wt = 0;
+    for(int i = 1; i < n; i++) {
+        proc[i].wt = proc[i].tat - proc[i].bt;
     }
+}
+
+void find_FCFS(Process proc[], int n) {
+    find_ct(proc, n);
+    find_tat(proc, n);
+    find_wt(proc, n);
+}
+
+void print_FCFS(Process proc[], int n) {
+    cout << "FCFS SCHEDULING......" << endl;
+    cout << "pid\tat\tbt\tct\ttat\twt" << endl;
+    for(int i = 0; i < n; i++) {
+        cout <<proc[i].pid<<"\t"<<proc[i].at<<"\t"<<proc[i].bt<<"\t"<<proc[i].ct<<"\t"<<proc[i].tat<<"\t"<<proc[i].wt<< endl;
+    }
+}
+int main() {
+    int n = 5;
+    Process proc[n] = {{1,0,5}, {2,1,3}, {3,2,2}, {4,3,4}, {5,4,1}};
+    find_FCFS(proc, n);
+    print_FCFS(proc, n);
 
     return 0;
 }
